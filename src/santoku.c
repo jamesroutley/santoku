b0a21c79fd309879c7d9b88d2008f4370d498df8
James Routley <jroutley@gmail.com>
2017-07-16 22:30:09 -0400

    Add support for variable arguments

--- src/santoku.c
+++ src/santoku.c
@@ -531,6 +531,24 @@ lval* lval_call(lenv* e, lval* f, lval* a) {
         // Pop the first symbol from the formals
         lval* sym = lval_pop(f->formals, 0);
 
+        // Check for &, which indicates a function with a variable number of
+        // arguments
+        if (strcmp(sym->sym, "&") == 0) {
+            // Ensure '&' is followed by another symbol
+            if (f->formals->count != 1) {
+                lval_del(a);
+                return lval_err("function format invalid. "
+                    "Symbol '&' not followed by a single symbol");
+            }
+
+            // Next formal should be bound to the remaining arguments
+            lval* nsym = lval_pop(f->formals, 0);
+            lenv_put(f->env, nsym, builtin_list(e, a));
+            lval_del(sym);
+            lval_del(nsym);
+            break;
+        }
+
         // Pop the next argument from the list
         lval* val = lval_pop(a, 0);
 
@@ -543,6 +561,29 @@ lval* lval_call(lenv* e, lval* f, lval* a) {
 
     // Argument list is now bound, so it can be cleaned up
     lval_del(a);
+
+    // If '&' remains in the formal list, bind to an empty list
+    if (f->formals->count > 0 &&
+            strcmp(f->formals->cell[0]->sym, "&") == 0) {
+
+        // Check to ensure '&' is not passed invalidly
+        if (f->formals->count != 2) {
+            return lval_err("function format invalid. "
+                "Symbol '&' not followed by a single symbol");
+        }
+
+        // Pop and delete '&'
+        lval_del(lval_pop(f->formals, 0));
+
+        // Pop next symbol and create empty list
+        lval* sym = lval_pop(f->formals, 0);
+        lval* val = lval_qexpr();
+
+        // Bind to environment and delete
+        lenv_put(f->env, sym, val);
+        lval_del(sym);
+        lval_del(val);
+    }
 
     // If all formals have been bound, evaluate
     if (f->formals->count == 0) {
