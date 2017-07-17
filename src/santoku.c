b2f28ed30cd77e7e53cad356762a8c655b202b4f
James Routley <jroutley@gmail.com>
2017-07-17 15:48:44 -0400

    Add support for comments

--- src/santoku.c
+++ src/santoku.c
@@ -167,6 +167,7 @@ int main(int argc, char** argv) {
     mpc_parser_t* Symbol = mpc_new("symbol");
     mpc_parser_t* Bool = mpc_new("bool");
     mpc_parser_t* String = mpc_new("string");
+    mpc_parser_t* Comment = mpc_new("comment");
     mpc_parser_t* Sexpr = mpc_new("sexpr");
     mpc_parser_t* Qexpr = mpc_new("qexpr");
     mpc_parser_t* Expr = mpc_new("expr");
@@ -179,13 +180,14 @@ int main(int argc, char** argv) {
             symbol  : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;            \
             bool    : /#[tf]/ ;                                     \
             string  : /\"(\\\\.|[^\"])*\"/ ;                        \
+            comment : /;[^\\r\\n]*/ ;                               \
             sexpr   : '(' <expr>* ')' ;                             \
             qexpr   : '{' <expr>* '}' ;                             \
             expr    : <number> | <symbol> | <bool> | <string>       \
-                    | <sexpr> | <qexpr> ;                           \
+                    | <comment> | <sexpr> | <qexpr> ;               \
             lispy   : /^/ <expr>* /$/ ;                             \
         ",
-        Number, Symbol, Bool, String, Sexpr, Qexpr, Expr, Lispy);
+        Number, Symbol, Bool, String, Comment, Sexpr, Qexpr, Expr, Lispy);
 
     puts("Lispy version 0.0.1");
     puts("Press ctrl+c to exit");
@@ -210,7 +212,8 @@ int main(int argc, char** argv) {
         }
         free(input);
     }
-    mpc_cleanup(5, Number, Symbol, Bool, String, Sexpr, Qexpr, Expr, Lispy);
+    mpc_cleanup(8, Number, Symbol, Bool, String, Comment, Sexpr, 
+        Qexpr, Expr, Lispy);
     return 0;
 }
 
@@ -539,6 +542,7 @@ lval* lval_read(mpc_ast_t* t) {
         if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
         if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
         if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
+        if (strstr(t->children[i]->tag, "comment")) { continue; }
         x = lval_add(x, lval_read(t->children[i]));
     }
     return x;
